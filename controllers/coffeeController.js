22b70d63a785ef166373fd26e79d7de978466ef9
createlsworklappy <justinecastillo7@gmail.com>
2026-02-27 10:37:27 +0900

    refactor(coffeeController): add controller operations for adding new coffee item via coffee-form page

--- controllers/coffeeController.js
+++ controllers/coffeeController.js
@@ -1,5 +1,7 @@
 import * as db from "../db/queries.js";
 
+//  -- COFFEE CATALOG PAGE --
+
 // render coffee-list catalog page
 export async function getCatalog(req, res) {
   try {
@@ -40,12 +42,51 @@ export async function getCoffeeDetails(req, res) {
       return res.status(404).send("Coffee not found");
     }
 
-    res.render('coffee-item', {
+    res.render("coffee-item", {
       title: `${coffee.origin_name} - ${coffee.name}`,
       coffee: coffee
     });
   } catch (error) {
     console.error("Error fetching coffee details.", error);
+    res.status(500).send("Internal Server Error");
+  }
+}
+
+// -- ADD NEW COFFEE PAGE -- 
+
+// render coffee-form to add new coffee item
+export async function getNewCoffeeForm(req, res) {
+  try {
+   const origins = await db.getAllOrigins();
+   
+   if (origins.length === 0) {
+    return res.status(404).send("Could not fetch list of origins.");
+   }
+
+   res.render("coffee-form", {
+    origins: origins
+   });
+  } catch (error) {
+    console.error("Error loading new coffee form.", error);
+    res.status(500).send("Internal Server Error");
+  }
+}
+
+// POST new coffee item into database
+export async function postNewCoffee(req, res) {
+  try {
+    const { name, location, description, price, stock, originId, } = req.body;
+
+    // check that mandatory fields not missing before insertion
+    if (!name || !price || !originId) {
+      return res.status(400).send("Name, price, and origin are required.");
+    }
+
+    await db.insertCoffee(name, location, description, price, stock, originId)
+
+    res.redirect("/catalog");
+  } catch (error) {
+    console.error("Error adding new item into the database.", error);
     res.status(500).send("Internal Server Error");
   }
 }
