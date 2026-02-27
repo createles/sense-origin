bb6f499620d1832dc13e59425520627c5dcfc033
createlsworklappy <justinecastillo7@gmail.com>
2026-02-27 16:03:38 +0900

    refactor(appRouter): add edit routes for coffee items

--- routes/appRouter.js
+++ routes/appRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getCatalog, getNewCoffeeForm, postNewCoffee } from "../controllers/coffeeController.js";
+import { getCatalog, getEditCoffeeForm, getNewCoffeeForm, postEditCoffee, postNewCoffee } from "../controllers/coffeeController.js";
 
 const appRouter = Router();
 
@@ -11,6 +11,9 @@ appRouter.get("/catalog", getCatalog);
 
 appRouter.get("/coffee-form", getNewCoffeeForm);
 appRouter.post("/coffee-form", postNewCoffee);
+
+appRouter.get("/coffee/:id/edit", getEditCoffeeForm);
+appRouter.post("/coffee/:id/edit", postEditCoffee);
 
 appRouter.get("/origin/:categoryName", (req, res) => {
   // temp basic use of route parameters and res.send
