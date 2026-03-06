f0a83c0154b21fc9ff4a02535d6625e28dd862c5
createlsworklappy <justinecastillo7@gmail.com>
2026-03-06 13:40:27 +0900

    feat(appRouter): use validation middleware for origin and coffee routes
    attached the validateOrigin and validateCoffee middleware to their respective routes

--- routes/appRouter.js
+++ routes/appRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getCatalog, getCoffeeDetails, getManagementDashboard, postDeleteCoffee, postDeleteOrigin, postEditCoffee, postEditOrigin, postNewCoffee, postNewOrigin } from "../controllers/coffeeController.js";
+import { getCatalog, getCoffeeDetails, getManagementDashboard, postDeleteCoffee, postDeleteOrigin, postEditCoffee, postEditOrigin, postNewCoffee, postNewOrigin, validateCoffee, validateOrigin } from "../controllers/coffeeController.js";
 
 const appRouter = Router();
 
@@ -17,13 +17,13 @@ appRouter.get("/coffee/:id", getCoffeeDetails);
 appRouter.get("/manage", getManagementDashboard);
 
 // ORIGIN ROUTES
-appRouter.post("/origin/new", postNewOrigin) // Create new origin
-appRouter.post("/origin/:id/edit", postEditOrigin); // Edit origin route
+appRouter.post("/origin/new", validateOrigin, postNewOrigin) // Create new origin
+appRouter.post("/origin/:id/edit", validateOrigin, postEditOrigin); // Edit origin route
 appRouter.post("/origin/:id/delete", postDeleteOrigin); // Delete origin route
 
 // COFFEE ROUTES
-appRouter.post("/coffee/new", postNewCoffee); // Create new coffee item
-appRouter.post("/coffee/:id/edit", postEditCoffee); // Edit coffee item routes
+appRouter.post("/coffee/new", validateCoffee, postNewCoffee); // Create new coffee item
+appRouter.post("/coffee/:id/edit", validateCoffee, postEditCoffee); // Edit coffee item routes
 appRouter.post("/coffee/:id/delete", postDeleteCoffee); // Delete coffee item route
 
 
