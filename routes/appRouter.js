fcf739859a4cbe89c6c53921133de7f8fde4f9e5
createlsworklappy <justinecastillo7@gmail.com>
2026-03-06 14:39:54 +0900

    feat(routes): add admin access routes and run admin check per CRUD route
    created the login and logout routes and passed requireAdmin helper to check whether users have admin privileges before running a CRUD operation via the relevant routes

--- routes/appRouter.js
+++ routes/appRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getCatalog, getCoffeeDetails, getManagementDashboard, postDeleteCoffee, postDeleteOrigin, postEditCoffee, postEditOrigin, postNewCoffee, postNewOrigin, validateCoffee, validateOrigin } from "../controllers/coffeeController.js";
+import { getCatalog, getCoffeeDetails, getLogin, getManagementDashboard, postDeleteCoffee, postDeleteOrigin, postEditCoffee, postEditOrigin, postLogin, postLogout, postNewCoffee, postNewOrigin, requireAdmin, validateCoffee, validateOrigin } from "../controllers/coffeeController.js";
 
 const appRouter = Router();
 
@@ -7,24 +7,30 @@ appRouter.get("/", (req, res) => {
   res.render("index");
 });
 
+// Login routes
+appRouter.get("/login", getLogin);
+appRouter.post("/login", postLogin);
+appRouter.post("/logout", postLogout);
+
+// Admin Management Dashboard route
+appRouter.get("/manage", requireAdmin, getManagementDashboard);
+
+
 // View coffee catalog
 appRouter.get("/catalog", getCatalog);
 
 // View coffee item details page
 appRouter.get("/coffee/:id", getCoffeeDetails);
 
-// Admin Management Dashboard route
-appRouter.get("/manage", getManagementDashboard);
-
 // ORIGIN ROUTES
-appRouter.post("/origin/new", validateOrigin, postNewOrigin) // Create new origin
-appRouter.post("/origin/:id/edit", validateOrigin, postEditOrigin); // Edit origin route
-appRouter.post("/origin/:id/delete", postDeleteOrigin); // Delete origin route
+appRouter.post("/origin/new", requireAdmin, validateOrigin, postNewOrigin) // Create new origin
+appRouter.post("/origin/:id/edit", requireAdmin, validateOrigin, postEditOrigin); // Edit origin route
+appRouter.post("/origin/:id/delete", requireAdmin, postDeleteOrigin); // Delete origin route
 
 // COFFEE ROUTES
-appRouter.post("/coffee/new", validateCoffee, postNewCoffee); // Create new coffee item
-appRouter.post("/coffee/:id/edit", validateCoffee, postEditCoffee); // Edit coffee item routes
-appRouter.post("/coffee/:id/delete", postDeleteCoffee); // Delete coffee item route
+appRouter.post("/coffee/new", requireAdmin, validateCoffee, postNewCoffee); // Create new coffee item
+appRouter.post("/coffee/:id/edit", requireAdmin, validateCoffee, postEditCoffee); // Edit coffee item routes
+appRouter.post("/coffee/:id/delete", requireAdmin, postDeleteCoffee); // Delete coffee item route
 
 
 export default appRouter;
